97648140ca1dc2e81607e18740b3cebdff133bee
allrounder745 <princekumarjha745@gmail.com>
2025-06-13 15:51:21 +0530

    Create score.js

---
+++ api/score.js
@@ -0,0 +1,7 @@
+export default async function handler(req, res) {
+  const response = await fetch("https://allrounderscoreboard.onrender.com/score?id=106673");
+  const data = await response.json();
+  
+  res.setHeader('Cache-Control', 'no-store');
+  res.status(200).json(data);
+}
